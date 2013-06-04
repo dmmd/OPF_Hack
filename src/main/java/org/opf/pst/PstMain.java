39b2bed9cef603defeb6b4bc71d5d4175ed278e6
Donald Mennerich <donaldmennerich@nypl.org>
2013-06-04 10:52:46 -0400

    fixing build script

--- src/main/java/org/opf/pst/PstMain.java
+++ src/main/java/org/opf/pst/PstMain.java
@@ -14,7 +14,7 @@ import org.apache.tika.parser.ParseContext;
 
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
-public class PSTMain {
+public class PstMain {
     public static void main(String[] args) throws IOException, SAXException, TikaException{
         System.out.println("PST Tika");
         File file = new File("/Users/dm/Desktop/outlook.pst");
